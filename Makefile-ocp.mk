27c8e19b916086096c2146e6bd1e16cc07723bfc
Kaustubh Pande <kaustubhpande.kp@gmail.com>
2026-04-14 14:33:53 +0530

    Add downstream Dockerfile

--- Makefile-ocp.mk
+++ Makefile-ocp.mk
@@ -1,5 +1,5 @@
 include Makefile
 
 .PHONY: build-ocp
-build-ocp:
-	CGO_ENABLED=0 go build -mod=vendor -a -o manager ./cmd
+build-ocp: clean fmt
+	CGO_ENABLED=1 $(GO_BUILD_ENV) go build $(COMMON_BUILD_ARGS) -tags=strictfipsruntime -mod=vendor -a -o manager ./cmd
