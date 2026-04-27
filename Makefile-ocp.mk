33d5bfca4ad96551b9a10751867cc91a3b660edd
Kaustubh Pande <kaustubhpande.kp@gmail.com>
2026-04-27 18:12:40 +0530

    Update prod image at dist bundle & Add Konflux release related configs

--- Makefile-ocp.mk
+++ Makefile-ocp.mk
@@ -3,3 +3,15 @@ include Makefile
 .PHONY: build-ocp
 build-ocp: clean fmt
 	CGO_ENABLED=1 $(GO_BUILD_ENV) go build $(COMMON_BUILD_ARGS) -tags=strictfipsruntime -mod=vendor -a -o manager ./cmd
+
+apply-override-snapshot:
+	oc apply -f .konflux_release/snapshot
+.PHONY: apply-override-snapshot
+
+apply-releaseplan:
+	oc apply -f .konflux_release/releasePlan
+.PHONY: apply-releaseplan
+
+apply-releases:
+	oc apply -f .konflux_release/releases
+.PHONY: apply-releases
